04b3ec5199917fdef15ba9f6669328900496d2a5
Brian Di Palma <offler@gmail.com>
2015-02-22 21:11:44 +0000

    Rename the test file.

--- test/default-formatter-test.js
+++ test/default-formatter-test.js
@@ -1,9 +1,14 @@
 import {describe, it} from 'mocha';
 
+import {defaultFormatCode} from '../src/index';
+
 describe('js-formatter', () => {
 	it('formats JS code according to config', () => {
 		// Given
+		// Preformatted code
 		// When
+		const formattedCode = defaultFormatCode('');
+
 		// Then
 	});
 });
