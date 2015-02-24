9dc34d1e58f49c5c6b2e7fd7ec20720380389c74
Brian Di Palma <offler@gmail.com>
2015-02-24 21:44:29 +0000

    Add a defaultFormatCode function to format code according to default configuration.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import winston from 'winston';
+import {format} from 'esformatter';
+
+import defaultConfig from '../esformatter.json';
+
+/**
+ * Format code based on the default configuration.
+ *
+ * @param   {string} codeToFormat Source code to format
+ * @returns {string} Formatted code
+ */
+export function defaultFormatCode(codeToFormat) {
+	return formatCode(codeToFormat, defaultConfig);
+}
+
 /**
  * Format code into a consistent style if the code is valid top level script code. It might not be
  * valid if, for example, it has an early return statement which is valid to have in function level
@@ -7,13 +22,13 @@
  * @param   {?Object} formatterOptions Options to configure formatting
  * @returns {string}  Formatted code
  */
-function formatCodeIfValid(codeToFormat, formatterOptions) {
+export function formatCode(codeToFormat, formatterOptions) {
 	try {
-		return esformatter.format(codeToFormat, formatterOptions);
+		return format(codeToFormat, formatterOptions);
 	} catch (error) {
-		console.error(codeToFormat);
-		console.error(error);
-		console.error('Error formatting source code, skipping formatting.');
+		winston.error(codeToFormat);
+		winston.error(error);
+		winston.error('Error formatting source code, skipping formatting.');
 	}
 
 	return codeToFormat;
