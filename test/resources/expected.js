85969956af06c70cff341e4a17d7eb5ca671bb76
Brian Di Palma <offler@gmail.com>
2015-02-28 12:21:39 +0000

    Add more checks for testing.

--- test/resources/expected.js
+++ test/resources/expected.js
@@ -12,5 +12,16 @@ function ApplicationMenu() {
 ApplicationMenu.prototype.method = function() {
 	this._serviceContainer[serviceName].containers.forEach(function(container) {
 		var menuItem = ElementUtility.getElementsByClassName(container, 'ul', 'menu-items')[0];
+		// comment
 	});
+};
+
+ApplicationMenu.prototype.aMethod = function() {
+	for (var i = 0; i < editIcons.children.length; ++i) {
+		editIcons.children[i].style.display = 'none';
+	}
+
+	editIcons.insertBefore(confirmIcon, editIcons.firstChild);
+
+	var textInput = ElementUtility.getElementsByClassName(container, 'input', 'rename-input')[0];
 };
