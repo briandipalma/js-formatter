6cc719f8818b3fb0fbdcca75ddf363e971716f6c
Brian Di Palma <offler@gmail.com>
2015-02-28 11:28:03 +0000

    Add more style checks.

--- test/resources/expected.js
+++ test/resources/expected.js
@@ -1,3 +1,16 @@
 'use strict';
 
 var emitr = require('emitr');
+
+function ApplicationMenu() {
+	this.EVENTS = {
+		SHOW: 'show',
+		HIDE: 'hide'
+	};
+}
+
+ApplicationMenu.prototype.method = function() {
+	this._serviceContainer[serviceName].containers.forEach(function(container) {
+		var menuItem = ElementUtility.getElementsByClassName(container, 'ul', 'menu-items')[0];
+	});
+};
