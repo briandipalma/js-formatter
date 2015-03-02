9b41fdd318bde707d9eeabe90be528d7ff9f28ec
Brian Di Palma <offler@gmail.com>
2015-03-02 08:45:11 +0000

    Move given into test spec.

--- test/default-formatter-test.js
+++ test/default-formatter-test.js
@@ -7,15 +7,17 @@ import {defaultFormatCode} from '../src/index';
 
 const fileOptions = {encoding: 'utf-8'};
 const testResourcesLocation = 'test/resources/';
-const givenCode = readFileSync(testResourcesLocation + 'given.js', fileOptions);
-const expectedCode = readFileSync(testResourcesLocation + 'expected.js', fileOptions);
 
 describe('js-formatter', () => {
 	it('formats JS code according to config', () => {
+		// Given
+		const givenCode = readFileSync(testResourcesLocation + 'given.js', fileOptions);
+
 		// When
 		const formattedCode = defaultFormatCode(givenCode);
 
 		// Then
+		const expectedCode = readFileSync(testResourcesLocation + 'expected.js', fileOptions);
 		equal(formattedCode, expectedCode);
 	});
 });
